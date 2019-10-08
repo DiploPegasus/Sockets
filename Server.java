49c9eb4a7c70f0ec6be6dee768c841547290a942
DiploPegasus <27516784@Student.g.nwu.ac.za>
2019-10-08 12:22:58 +0200

    Updated Server

--- Server.java
+++ Server.java
@@ -8,6 +8,43 @@ public class Server
 	private Socket socket = null;
 	private ServerSocket server = null;
 	private DataInputStream in = null;
+
+	public Server(int port)
+	{
+		try
+		{
+			server = new ServerSocket(port);
+			System.out.println("Server Started");
+
+			System.out.println("Waiting for a client to connect...");
+
+			socket = server.accept();
+			System.out.println("Client connetion accepted");
+
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			String line = "";
+
+			While(!line.equals("Done"))
+			{
+				try
+				{
+					line = in.readUtf();
+					System.out.println(line);
+				}
+				catch(IOException i)
+				{
+					System.out.println(i);
+
+				}
+			}
+			socket.close();
+			in.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
 }
 
     public static void main(String args[])
